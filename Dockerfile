be4e67af24c84eeff04dec6b9bbf21942569a475
Parnish Sharma <90691906+ParnishSharma@users.noreply.github.com>
2023-04-09 16:35:36 +0530

    rndr file

--- Dockerfile
+++ Dockerfile
@@ -6,10 +6,11 @@ WORKDIR /app
 
 COPY requirements.txt .
 
-RUN pip install --no-cache-dir -r requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt && \
+    pip install gunicorn
 
 COPY . .
 
 EXPOSE 5000
 
-CMD ["python", "app.py"]
+CMD ["gunicorn", "app:app", "-b", "0.0.0.0:5000"]
--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, jsonify, request
 import requests
+
+
 import os
 
 app = Flask(__name__)
@@ -52,4 +54,5 @@ def search():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
